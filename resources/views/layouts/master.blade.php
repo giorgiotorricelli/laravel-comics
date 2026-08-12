40b447f45ceb9bbd13638c40b3c2db557fc04f29
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-08-12 18:30:03 +0200

    creato layout master, aggiunto bootstrap, creata nav bar con link funzionanti e logo

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <header class="d-flex justify-content-around align-items-center">
+        <div class="logo-wrapper">
+            <img class="logo" src="{{ asset("img/dc-logo (2).png") }}" alt="Logo">
+        </div>
+            <ul class="d-flex nav-list align-items-stretch">
+                <li class="{{ request()->routeIs('characters') ? 'active' : '' }}"><a href="{{ route('characters') }}">CHARACTERS</a></li>
+                <li class="{{ request()->routeIs('comics') ? 'active' : '' }}"><a href="{{ route('comics') }}">COMICS</a></li>
+                <li class="{{ request()->routeIs('movies') ? 'active' : '' }}"><a href="{{ route('movies') }}">MOVIES</a></li>
+                <li class="{{ request()->routeIs('tv') ? 'active' : '' }}"><a href="{{ route('tv') }}">TV</a></li>
+                <li class="{{ request()->routeIs('games') ? 'active' : '' }}"><a href="{{ route('games') }}">GAMES</a></li>
+                <li class="{{ request()->routeIs('collectibles') ? 'active' : '' }}"><a href="{{ route('collectibles') }}">COLLECTIBLES</a></li>
+                <li class="{{ request()->routeIs('videos') ? 'active' : '' }}"><a href="{{ route('videos') }}">VIDEOS</a></li>
+                <li class="{{ request()->routeIs('fans') ? 'active' : '' }}"><a href="{{ route('fans') }}">FANS</a></li>
+                <li class="{{ request()->routeIs('news') ? 'active' : '' }}"><a href="{{ route('news') }}">NEWS</a></li>
+                <li class="{{ request()->routeIs('shop') ? 'active' : '' }}"><a href="{{ route('shop') }}">SHOP</a></li>
+            </ul>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <div class="container">
+            <h1>footer</h1>
+        </div>
+    </footer>
+</body>
+</html>
